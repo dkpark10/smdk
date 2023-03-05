96cf6ae62d45c43e71289dbdc12b3d7a64e9fbe5
dkpark10 <dkpark10@naver.com>
2023-03-05 22:55:28 +0900

    feat: 채팅 모킹데이터 로직 추가

--- client/generate_mock.js
+++ client/generate_mock.js
@@ -1,6 +1,45 @@
-import { writeFileSync } from 'fs';
-import path from 'path';
+import { appendFileSync } from "fs";
+import { faker } from "@faker-js/faker";
+import path from "path";
+
+const __dirname = path.resolve();
+
+/**
+ * @param {number} value 
+ * @return {string}
+ */
+const addPrefixZero = (value) => {
+  return value < 10 ? `0${value}` : String(value);
+}
+
+/**
+ * @param {string} hour 
+ * @return {string}
+ */
+const checkAfternoon = (hour) => {
+  return hour >= 12 ? '오후' : '오전';
+}
+
+/**
+ * @return {string}
+ */
+const getChatDate = () => {
+  const hour = addPrefixZero(new Date().getHours());
+  const minute = addPrefixZero(new Date().getMinutes());
+
+  return `${checkAfternoon(hour)}: ${hour}:${minute}`;
+}
 
 (function () {
   const COUNT_CREATED_CHAT = 20;
+  const newChat = [];
+
+  for (let i = 0; i < COUNT_CREATED_CHAT; i += 1) {
+    newChat.push({
+      content: faker.datatype.uuid(),
+      date: getChatDate(),
+    });
+  }
+
+  appendFileSync(path.join(__dirname, 'src/mock/chat_mock.json'), JSON.stringify(newChat));
 })();
