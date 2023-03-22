29c3310093344839fc984609c6f183e16febcf54
dkpark10 <dkpark10@naver.com>
2023-03-22 22:43:17 +0900

    chore: 린트 설치 및 설정

--- client/src/services/queryfetch.ts
+++ client/src/services/queryfetch.ts
@@ -1,6 +1,8 @@
 import { useQuery, QueryOptions } from "@tanstack/react-query";
 import { fetchClient } from "../utils/fetchclient";
 
+const title = '123123';
+
 export const useFetchQuery = async <T>(queryKey: string[], url: string, queryOptions?: QueryOptions) => {
   return useQuery(queryKey, () => fetchClient.get<T>(url), queryOptions);
 }
