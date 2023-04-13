ea952d99b3ab23be7f6a9689c92cd97e2617af0a
dkpark10 <dkpark10@naver.com>
2023-04-13 18:28:54 +0900

    chore: 공통 패키지 정리

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,9 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-if (process.env.NEXT_PUBLIC_API_MOCKING === "enalbed") {
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enalbed') {
   import('../mock');
 }
 
@@ -11,11 +10,8 @@ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={true} />
-          <Component {...pageProps} />
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
   );
-};
+}
