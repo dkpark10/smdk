50b26ff92134cca6cd36e2f5f84da28487f6a3c7
dkpark10 <dkpark10@naver.com>
2023-04-30 02:12:49 +0900

    feat: 서버 간단 보안 설정

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import csurf from 'csurf';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+(async function run() {
+  /**
+   * @todo cors 설정 디테일하게
+   */
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
+  app.use(csurf());
   app.useWebSocketAdapter(new WsAdapter(app));
 
   await app.listen(8080);
   console.log(`Application is running on: ${await app.getUrl()}`);
-}
-bootstrap();
+})();
